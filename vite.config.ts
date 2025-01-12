d4351054e31c6c6dd00f0ba3bff799926322b0f7
DevTeddy123 <ab315601@naver.com>
2025-01-12 16:15:10 +0900

    Fix : issue check

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/lib/index.tsx'),
+			name: 'ReactAnimateMagic',
+			fileName: 'index',
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+		commonjsOptions: {
+			esmExternals: ['react', 'react-dom'],
+		},
+	},
+	plugins: [react()],
 })
